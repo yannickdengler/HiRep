3e667a6a4a2445f3907e1694d24133e7ce7b09f8
Michele Mesiti <michele.mesiti@gmail.com>
2020-08-07 14:22:46 +0100

    Fixed - for spn - TestProgram/Utils/check_suNg_op

--- TestProgram/Utils/check_suNg_op.c
+++ TestProgram/Utils/check_suNg_op.c
@@ -17,6 +17,12 @@
 #include "representation.h"
 #include "utils.h"
 
+#ifdef GAUGE_SPN
+#define _NROWS NG/2
+#else
+#define _NROWS NG
+#endif
+
 void print_cmplx(double complex c){
   printf("%g",creal(c));
   if (cimag(c)>0){
@@ -30,7 +36,7 @@ void print_cmplx(double complex c){
 double  norm_suNg_minus_id(suNg* a){
   int i,j;
   double r=0.;
-  for (i=0;i<NG;i++){
+  for (i=0;i<_NROWS;i++){
       for (j=1;j<NG;j++){
         #ifdef WITH_QUATERNIONS
         r +=fabs(a->c[i*NG+j]);
@@ -46,7 +52,7 @@ return r;
 void printML(suNg* a){
   int i,j;
   printf("[");
-  for (i=0;i<NG;i++){
+  for (i=0;i<_NROWS;i++){
     print_cmplx(a->c[i*NG]);
     for (j=1;j<NG;j++){
       printf(",");
@@ -84,26 +90,32 @@ int main(){
   inv_hermNg(&a); /* a= (a+b)^-1 does not work for SO ? */
   printML(&a);
   _suNg_times_suNg(b,a,c); /* b = (a+b)^-1 *(a+b) so it should be 1 */
-  printf("Should be the idendity matrix:\n");
+  printf("Should be the identity matrix:\n");
   printML(&b);
   test= norm_suNg_minus_id(&b);
-  if (test > 1e-14) return_value +=1;
+  if (test > 1e-14){
+    printf("Deviation from the identity matrix: %f\n",test);
+    return_value +=1;
+  }
 
   c=b; /* c =  (a+b)^-1 *(a+b) */
   printML(&b); /* Vincent useless */
   inv_hermNg(&b); /* b = ((a+b)^-1 *(a+b))^-1 */
   det_hermNg(&det,&b);
-  printf("Should be the determinant of the idendity matrix:\n");
+  printf("Should be the determinant of the identity matrix:\n");
   printf("Det: ");
   print_cmplx(det);
   if (cabs(det - 1.0) >1e-14) return_value +=1;
   printf("\n");
   printML(&b);
   _suNg_times_suNg(a,b,c); /* a = ((a+b)^-1 *(a+b))^-1  * (a+b)^-1 *(a+b) */
-  printf("Should be the idendity matrix:\n");
+  printf("Should be the identity matrix:\n");
   printML(&a);
   test= norm_suNg_minus_id(&a);
-  if (test > 1e-14) return_value +=1;
+  if (test > 1e-14){
+    printf("Deviation from the identity matrix: %f\n",test);
+    return_value +=1;
+  }
 
 
   return return_value;
