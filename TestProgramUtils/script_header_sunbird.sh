f5acaee0c379c35d011d67c49d3f56aeba2438f7
Michele Mesiti <michele.mesiti@gmail.com>
2021-01-15 09:31:43 +0000

    TestProgram/_utils -> TestProgramUtils
    otherwise run_tests.sh trips

--- TestProgramUtils/script_header_sunbird.sh
+++ TestProgramUtils/script_header_sunbird.sh
@@ -4,8 +4,8 @@
 #SBATCH -A scw1019
 #SBATCH -t 20
 #SBATCH -J HRTS # HiRep Test Suite
-#SBTACH -o outSED_I.txt
-#SBTACH -e errSED_I.txt
+#SBATCH -o outSED_I.txt
+#SBATCH -e errSED_I.txt
 
 module load compiler/intel/2019
 module load mpi/intel/2019
