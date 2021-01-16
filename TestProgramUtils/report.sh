76b376079a36f8edf103a62cb5fc639d4c68f052
Michele Mesiti <michele.mesiti@gmail.com>
2021-01-16 19:59:34 +0000

    Better reporting of test errors.

--- TestProgramUtils/report.sh
+++ TestProgramUtils/report.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
+BOLD="\e[0;1m"
+NORM="\e[0;0m"
+BOLDGREEN="\e[1;32m"
+BOLDRED="\e[1;31m"
+
 get_options(){
     local FILENAME=$1
-    head -n 1 $FILENAME | cut -d':' -f2
+    echo -e $BOLD $(head -n 1 $FILENAME | cut -d':' -f2) $NORM
 }
+
 
 check_ok(){
     local FILENAME=$1
     if grep -i fail $FILENAME &> /dev/null
     then
-        echo FAIL
-        grep -i fail $FILENAME
+        echo -e $BOLDRED FAIL $NORM
+        echo "=> What failed:"
+        grep -i fail $FILENAME | grep '\.\.\.'
     else
-        echo OK
+        echo -e $BOLDGREEN OK $NORM
     fi
 }
 
 analyse_file(){
     local FILENAME=$1
-    printf "%s %s\n" "$(get_options $FILENAME )" "$(check_ok $FILENAME)"
+    printf "## %s %s\n" "$(get_options $FILENAME )" "$(check_ok $FILENAME)"
 }
 
 for FILENAME in out*.txt
